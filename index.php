26622867944b17c65ca5014dfe56caccacf2d2d3
PeterPL-JR <73580223+PeterPL-JR@users.noreply.github.com>
2022-06-16 15:03:35 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -3,30 +3,44 @@
 <head>
     <meta charset="UTF-8">
     <title>Mundial 2022 - Wytypuj Wyniki</title>
+    <script src="library.js"></script>
+
     <link rel="icon" href="images/logo.png" type="image/png">
     <link rel="stylesheet" href="styles/basic-styles.css">
+    <link rel="stylesheet" href="styles/game-styles.css">
+    <link rel="stylesheet" href="styles/matches-styles.css">
+
+    <link rel="stylesheet" href="styles/group-table-styles.css">
+    <link rel="stylesheet" href="styles/matches-table-styles.css">
+    <link rel="stylesheet" href="styles/share-styles.css">
 </head>
 <body>
 <img src="images/background.png" id="background">
 <div id="container">
-    <img src="images/logo.png" id="logo">
-    <div id="content">
+    <div id="header-div">
+        <img src="images/logo.png" id="logo">
         <div id="logo-name">Mundial 2022</div>
-
-        <div id="description">
-            <p>Wytypuj Wyniki</p>
-            <div></div>
-            Strona ta umożliwi ci w łatwy sposób wytypować wyniki
-            <span>Mundialu 2022</span>
-        </div>
-        <div id="button-div">
-            <img src="images/logo-title.png">
-            <a href="play.php">
-                <button class="button">Rozpocznij</button>
-            </a>
-        </div>
         <div style="clear: both;"></div>
     </div>
+    <div id="content">
+        <div class="round-name">
+            <img src="images/ball.png">
+            <div class="name-div"></div>
+            <div style="clear: both"></div>
+        </div>
+    </div>
 </div>
+<script>
+
+    var teams;
+    serverGet("/create-mundial/get_data.php", null, function(text) {
+        teams = JSON.parse(text);
+        createScript("prepare-groups.js");
+        createScript("group-round-handler.js");
+
+        createScript("shared-obj-handler.js");
+        createScript("knock-round-handler.js");
+    });
+</script>
 </body>
 </html>
